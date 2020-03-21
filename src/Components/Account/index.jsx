6715d0256edff193dc3eb884de1189b9944925a4
Alex Basystyi <alexbasisty@gmail.com>
2020-03-21 15:39:47 +0100

    password change form

--- src/Components/Account/index.jsx
+++ src/Components/Account/index.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import { PasswordForgetForm } from '../PasswordForget';
+import PasswordChangeForm from '../PasswordChange';
 
-const AccountPage = () => null;
+const AccountPage = () => (
+    <div>
+      <h1>Account Page</h1>
+      <PasswordForgetForm />
+      <PasswordChangeForm />
+    </div>
+);
 
 export default AccountPage;
