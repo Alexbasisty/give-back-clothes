8a4e7877c7e76f9e1e80cbfc6d77febd6b1d8dc7
Alex Basystyi <alexbasisty@gmail.com>
2020-03-21 13:06:33 +0100

    FIREBASE'S AUTHENTICATION API

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
-
+import Firebase, { FirebaseContext } from "./Components/Firebase";
 import 'normalize.css';
 import './scss/main.scss';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseContext.Provider value={new Firebase()}>
     <App />
-  </React.StrictMode>,
+  </FirebaseContext.Provider>,
   document.getElementById('root')
 );
 
