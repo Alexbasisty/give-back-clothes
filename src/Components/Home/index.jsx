2d132be42340ffb18b2f5c51d494a22237edd013
Alex Basystyi <alexbasisty@gmail.com>
2020-03-22 17:41:00 +0100

    reset css installed

--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import HomeHeader from "./homeheader";
+import ThreeColumns from "./threecolumns";
 
 const HomePage = () => (
     <>
       <HomeHeader />
+      <ThreeColumns/>
     </>
 );
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './Components/App/App';
 import Firebase, { FirebaseContext } from "./Components/Firebase";
-import 'normalize.css';
+import { Reset } from 'styled-reset';
 import './scss/main.scss';
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
+    <Reset />
     <App />
   </FirebaseContext.Provider>,
   document.getElementById('root')
