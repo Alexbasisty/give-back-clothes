5d59f6ffe37b993ec8defa799b420e9655cfda37
Alex Basystyi <alexbasisty@gmail.com>
2020-03-23 10:28:55 +0100

    about us component

---
+++ src/Components/Home/AboutUs.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const AboutUs = () => (
+    <div id="aboutUs">
+
+    </div>
+);
+export default AboutUs;
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import Homeheader from "./Homeheader";
 import Threecolumns from "./Threecolumns";
 import SimpleSteps from "../SimpleSteps";
+import AboutUs from "./AboutUs";
 
 const HomePage = () => (
     <>
       <Homeheader />
       <Threecolumns/>
       <SimpleSteps />
+      <AboutUs />
     </>
 );
 
