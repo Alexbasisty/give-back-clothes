330520863d0c732e84a4c2e245367b9f3a09d994
Alex Basystyi <alexbasisty@gmail.com>
2020-03-23 12:05:46 +0100

    get data from fierbase testing

--- src/Components/Home/WhoWeHelp/index.jsx
+++ src/Components/Home/WhoWeHelp/index.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import app from 'firebase/app';
 
-const WhoWeHelp = () => (
-    <div id="foundations">Hello</div>
-);
+const WhoWeHelp = () => {
+
+ const getData = () => {
+   const foundations = app.database().ref('foundations');
+   foundations.on('value', data => {
+     console.log(data.val());
+   });
+  };
+ 
+  return (
+      <div id="foundations" onClick={getData}>Hello</div>
+
+  )
+};
 
 export default WhoWeHelp;
