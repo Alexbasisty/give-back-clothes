20ef82d3bfd25fd0a3cfa3552fcc50d97a132e2a
Alex Basystyi <alexbasisty@gmail.com>
2020-03-23 12:24:10 +0100

    file structure

--- src/Components/Home/WhoWeHelp/index.jsx
+++ src/Components/Home/WhoWeHelp/index.jsx
@@ -1,19 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
 import app from 'firebase/app';
 
-const WhoWeHelp = () => {
+class WhoWeHelp extends Component {
+  state = {
+    description: '',
+    title: '',
+    mission: '',
+    staff: ''
+      };
 
- const getData = () => {
+  getData = () => {
    const foundations = app.database().ref('foundations');
    foundations.on('value', data => {
      console.log(data.val());
    });
   };
- 
-  return (
-      <div id="foundations" onClick={getData}>Hello</div>
 
-  )
-};
+  render() {
+    return (
+      <div id="foundations" onClick={this.getData}>
+          <h2>Komu pomagamy?</h2>
+          <img src={require('../../../assets/Decoration.svg')} alt='decoration' />
+          <section className='links'>
+            <p>Fundacjom</p>
+            <p>Organizacjom pozarządowym</p>
+            <p>Lokalnym zbiórkom</p>
+          </section>
+        </div>
+
+    )                                                                               
+  }
+}
 
 export default WhoWeHelp;
