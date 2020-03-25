9da46f816eb0d0a5e7f7b7874658806cfbf42d62
Alex Basystyi <alexbasisty@gmail.com>
2020-03-25 12:16:30 +0100

    contact form background

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -6,7 +6,22 @@ class Contact extends Component {
   render() {
     return (
         <div id='contact' className="contact">
-          <Footer />
+          <div className='contact-form'>
+            <h1>Skontaktuj się z nami</h1>
+            <img src={require('../../assets/Decoration.svg')} />
+            <form>
+              <div>
+                <label>Wpisz swoje imię</label>
+                <input />
+                <label>Wpisz swój email</label>
+                <input />
+              </div>
+              <label>Wpisz swoją wiadomość</label>
+              <textarea />
+              <button>Wyślij</button>
+            </form>
+          </div>
+          <div><Footer /></div>
         </div>
     );
   }
