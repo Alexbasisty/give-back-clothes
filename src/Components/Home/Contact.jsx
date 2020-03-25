f26a81f1497b65b6b9e98ae3d00db33757ef322c
Alex Basystyi <alexbasisty@gmail.com>
2020-03-25 13:18:58 +0100

    contact form styling

--- src/Components/Home/Contact.jsx
+++ src/Components/Home/Contact.jsx
@@ -14,16 +14,20 @@ class Contact extends Component {
                 <img src={require('../../assets/Decoration.svg')} />
               </div>
             <form>
-              <div>
-                <label>Wpisz swoje imię</label>
-                <input placeholder='Krzysztof' />
-                <label>Wpisz swój email</label>
-                <input placeholder='abc@xyz.pl' />
+              <div className="contact-data">
+                <label>Wpisz swoje imię
+                  <input name='name' placeholder='Krzysztof' />
+                </label>
+                <label>Wpisz swój email
+                  <input name='mail' placeholder='abc@xyz.pl' />
+                </label>
               </div>
-              <label>Wpisz swoją wiadomość</label>
-              <textarea
+              <label className='message'>Wpisz swoją wiadomość
+                <textarea
+                  name='message'
                   placeholder='Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'
-              />
+                />
+              </label>
               <button>Wyślij</button>
             </form>
             </div>
