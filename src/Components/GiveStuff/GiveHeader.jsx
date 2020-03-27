0e2e56275c10c49c650f4b5f6a514c51a8767d15
Alex Basystyi <alexbasisty@gmail.com>
2020-03-27 12:24:59 +0100

    give form header

---
+++ src/Components/GiveStuff/GiveHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Homelinks from "../Home/Homelinks";
+
+const GiveHeader = () => (
+    <div className="home-header">
+      <div className="home-hero" />
+      <div className="home-title">
+        <div className="text">
+          <p style={{margin: 0}}>Oddaj rzeczy, których już nie chcesz <strong>potrzebującym</strong></p>
+        </div>
+        <div className="decoration" />
+      </div>
+    </div>
+);
+
+export default GiveHeader;
--- src/Components/GiveStuff/index.jsx
+++ src/Components/GiveStuff/index.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
 
+import Contact from "../Home/Contact";
+import GiveHeader from "./GiveHeader";
 
-const GiveStuff = () => null;
+const GiveStuff = () => (
+    <>
+      <GiveHeader />
+      <Contact />
+    </>
+);
 
 const GiveStuffLink = () => (
     <Link to={ROUTES.GIVE_STUFF}>Oddaj rzeczy</Link>
