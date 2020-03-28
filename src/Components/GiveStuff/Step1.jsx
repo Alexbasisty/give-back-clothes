260c2598d5cf32277f17ca30c49766164020f1dd
Alex Basystyi <alexbasisty@gmail.com>
2020-03-28 12:57:16 +0100

    step 1 styled

--- src/Components/GiveStuff/Step1.jsx
+++ src/Components/GiveStuff/Step1.jsx
@@ -1,46 +1,64 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ImportantField from "./ImportantField";
 
-const Step1 = () => (
-    <>
-      <ImportantField>
-        Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.
-      </ImportantField>
-      <div className="steps-form-field">
-        <h3>Krok 1/4</h3>
-        <h1>Zaznacz co chcesz oddać:</h1>
-        <form>
-          <label>
-          <input type="radio" checked="checked"/>
-            <span className="checkmark" />
-          ubrania, które nadają się do ponownego użycia
-          </label>
-          <label>
-            <input type="radio" />
-            <span className="checkmark" />
-            ubrania, do wyrzucenia
-          </label>
-          <label>
-            <input type="radio" />
-            <span className="checkmark" />
-            zabawki
-          </label>
-          <label>
-            <input type="radio" />
-            <span className="checkmark" />
-            książki
-          </label>
-          <label>
-            <input type="radio" />
-            <span className="checkmark" />
-            Inne
-          </label>
-        </form>
-        <button>
-          Dalej
-        </button>
-      </div>
-    </>
-);
+class Step1 extends Component {
+  state = {
+    isChecked: true
+  };
+
+  toggleChange = () => {
+    this.setState({
+      isChecked: !this.state.isChecked,
+    });
+  };
+
+
+  render() {
+    return (
+        <>
+          <ImportantField>
+            Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.
+          </ImportantField>
+          <div className="steps-form-field">
+            <h3>Krok 1/4</h3>
+            <h1>Zaznacz co chcesz oddać:</h1>
+            <form>
+              <label>
+                <input
+                    checked={this.state.isChecked}
+                    onChange={this.toggleChange}
+                    type="checkbox" />
+                <span className="checkmark"/>
+                ubrania, które nadają się do ponownego użycia
+              </label>
+              <label>
+                <input type="checkbox"/>
+                <span className="checkmark"/>
+                ubrania, do wyrzucenia
+              </label>
+              <label>
+                <input type="checkbox"/>
+                <span className="checkmark"/>
+                zabawki
+              </label>
+              <label>
+                <input type="checkbox"/>
+                <span className="checkmark"/>
+                książki
+              </label>
+              <label>
+                <input type="checkbox"/>
+                <span className="checkmark"/>
+                Inne
+              </label>
+            </form>
+            <button>
+              Dalej
+            </button>
+          </div>
+        </>
+    );
+  }
+}
 
 export default Step1;
