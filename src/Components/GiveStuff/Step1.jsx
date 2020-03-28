37f4dfa57cd9c6a3d8e114cd5a195bc87c78ed60
Alex Basystyi <alexbasisty@gmail.com>
2020-03-28 12:39:12 +0100

    radio inputs styling

--- src/Components/GiveStuff/Step1.jsx
+++ src/Components/GiveStuff/Step1.jsx
@@ -6,6 +6,40 @@ const Step1 = () => (
       <ImportantField>
         Uzupełnij szczegóły dotyczące Twoich rzeczy. Dzięki temu będziemy wiedzieć komu najlepiej je przekazać.
       </ImportantField>
+      <div className="steps-form-field">
+        <h3>Krok 1/4</h3>
+        <h1>Zaznacz co chcesz oddać:</h1>
+        <form>
+          <label>
+          <input type="radio" checked="checked"/>
+            <span className="checkmark" />
+          ubrania, które nadają się do ponownego użycia
+          </label>
+          <label>
+            <input type="radio" />
+            <span className="checkmark" />
+            ubrania, do wyrzucenia
+          </label>
+          <label>
+            <input type="radio" />
+            <span className="checkmark" />
+            zabawki
+          </label>
+          <label>
+            <input type="radio" />
+            <span className="checkmark" />
+            książki
+          </label>
+          <label>
+            <input type="radio" />
+            <span className="checkmark" />
+            Inne
+          </label>
+        </form>
+        <button>
+          Dalej
+        </button>
+      </div>
     </>
 );
 
