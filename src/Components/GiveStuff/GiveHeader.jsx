7dd5a957dbc6f3f34b3b4cbb2f58f213745c8de2
Alex Basystyi <alexbasisty@gmail.com>
2020-03-28 11:30:45 +0100

    give form header styling

--- src/Components/GiveStuff/GiveHeader.jsx
+++ src/Components/GiveStuff/GiveHeader.jsx
@@ -3,27 +3,29 @@ import React from 'react';
 const GiveHeader = () => (
     <div className="home-header give-stuff-header">
       <div className="home-hero" />
-      <div className="home-title">
+      <div className="home-title" style={{width: '760px'}}>
         <div className="text">
           <p style={{margin: 0, width: '602px'}}>Oddaj rzeczy, których już nie chcesz <strong>potrzebującym</strong></p>
         </div>
         <div className="decoration" />
         <h2>Wystarczą 4 proste kroki:</h2>
-        <div>
-          <h4>1</h4>
-          <p>Wybierz rzeczy</p>
-        </div>
-        <div>
-          <h4>2</h4>
-          <p>Spakuj je w worki</p>
-        </div>
-        <div>
-          <h4>3</h4>
-          <p>Wybirz fundację</p>
-        </div>
-        <div>
-          <h4>4</h4>
-          <p>Zamów kuriera</p>
+        <div className='header-steps'>
+          <div>
+            <h4>1</h4>
+            <p>Wybierz rzeczy</p>
+          </div>
+          <div>
+            <h4>2</h4>
+            <p>Spakuj je w worki</p>
+          </div>
+          <div>
+            <h4>3</h4>
+            <p>Wybierz fundację</p>
+          </div>
+          <div>
+            <h4>4</h4>
+            <p>Zamów kuriera</p>
+          </div>
         </div>
       </div>
     </div>
