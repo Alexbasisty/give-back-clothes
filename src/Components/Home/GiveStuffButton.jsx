1891ce6a0f552c15d0a9738a780ce36972f5e07f
Alex Basystyi <alexbasisty@gmail.com>
2020-04-05 16:21:34 +0200

    giveStuffButton link

--- src/Components/Home/GiveStuffButton.jsx
+++ src/Components/Home/GiveStuffButton.jsx
@@ -1,14 +1,34 @@
-import React, { Component } from 'react';
+import React from 'react';
 import {Link} from "react-router-dom";
 import * as ROUTES from "../../constants/routes";
+import { AuthUserContext } from '../Session';
 
-class GiveStuffButton extends Component{
+const GiveStuffButton = () => (
+    <AuthUserContext.Consumer>
+      {authUser =>
+          authUser ? (
+              <GiveStuffButtonAuth authUser={authUser} />
+          ) : (
+              <GiveStuffButtonNonAuth />
+          )
+      }
+    </AuthUserContext.Consumer>
+);
 
-  render() {
+const GiveStuffButtonNonAuth = () => {
+
     return (
           <Link className="home-links" to={ROUTES.SIGN_IN}>ODDAJ RZECZY</Link>
     );
-  }
-}
+};
+
+const GiveStuffButtonAuth = () => {
+
+  return (
+      <Link className="home-links" to={ROUTES.GIVE_STUFF}>ODDAJ RZECZY</Link>
+  );
+};
+
+
 
 export default GiveStuffButton;
